335951b9b32de859d7526da98cb71650f32fd4a4
jmaciasb <jmaciasb@BE401S103C26ING.unal.loc>
2019-05-06 08:37:29 -0500

    Ejercicio clase 06/05

---
+++ src/autoherencia/Auto.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package autoherencia;
+
+/**
+ *
+ * @author jmaciasb
+ */
+public class Auto {
+    protected String placa;
+    protected int nroSerie;
+    protected String marca;
+    protected int anio;
+    protected double precio;
+
+    public Auto(String placa, int nroSerie, String marca, int anio, double precio) {
+        this.placa = placa;
+        this.nroSerie = nroSerie;
+        this.marca = marca;
+        this.anio = anio;
+        this.precio = precio;
+    }
+
+    
+    
+    
+
+  
+                                                                           
+}
+    
